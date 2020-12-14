c40f9e1ff9e82256b45b74122d64176af2454c22
ahonore42 <a1honore80@gmail.com>
2020-12-13 21:35:05 -0600

    add information to movie page

--- src/components/pages/MoviePage.js
+++ src/components/pages/MoviePage.js
@@ -20,12 +20,20 @@ const MoviePage = (props) => {
                 <div>
                     <h2 style={{textAlign: 'right'}}>{movieState.movie.tagline==="" ? "About This Film" : movieState.movie.tagline}</h2>
                     <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <div>
-                            <img style={{width: 'auto', height: '40vh', boxShadow: '0px 0px 5px cyan'}} src={`https://image.tmdb.org/t/p/w500${movieState.movie.poster_path}`} alt="poster" />
-                            <h3>Runtime: {movieState.movie.runtime} minutes</h3>
-                            <h3>Release Date: {movieState.movie.release_date}</h3>
+                        <div style={{backgroundColor: '#fff', color: 'rgba(0,0,0,0.87)'}}>
+                            <img style={{width: 'auto', height: '50vh', boxShadow: '0px 0px 5px cyan'}} src={`https://image.tmdb.org/t/p/w500${movieState.movie.poster_path}`} alt="poster" />
+                            <div className='movie-info'>
+                                <span style={{width: '100%', margin: '0', display: 'flex', justifyContent: 'space-between'}}><p style={{fontWeight: '700'}}>Average Rating: </p><p>{movieState.movie.vote_average}/10</p></span>
+                                <span style={{width: '100%', margin: '0', display: 'flex', justifyContent: 'space-between'}}><p style={{fontWeight: '700'}}>Run Time: </p><p>{movieState.movie.runtime} minutes</p></span>
+                                <span style={{width: '100%', margin: '0', display: 'flex', justifyContent: 'space-between'}}><p style={{fontWeight: '700'}}>Release Date: </p><p>{movieState.movie.release_date}</p></span>
+                                <span style={{width: '100%', margin: '0', display: 'flex', justifyContent: 'space-between'}}><p style={{fontWeight: '700'}}>Budget: </p><p>${movieState.movie.budget}</p></span>
+                                <span style={{width: '100%', margin: '0', display: 'flex', justifyContent: 'space-between'}}><p style={{fontWeight: '700'}}>Revenue: </p><p>${movieState.movie.revenue}</p></span>
+                                <a href={`https://www.imdb.com/title/${movieState.movie.imdb_id}/`}>View on IMDB</a>
+                            </div>
                         </div>
-                        <div></div>
+                        <div style={{paddingLeft: '5vw'}}>
+                            <h3>{movieState.movie.overview}</h3>
+                        </div>
                     </div>
                 </div>
         
